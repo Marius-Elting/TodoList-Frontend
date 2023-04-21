99798c100a05f59dd01bc91be2828227df261b8d
Marius-Elting <Marius.Supercode@gmail.com>
2023-04-21 10:57:34 +0200

    project initialized & installed packages

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./App.css";
+import { FC, ReactElement } from "react";
+
+const App: FC = (): ReactElement => {
+  return <div></div>;
+};
+
+export default App;
