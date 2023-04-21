b5a3c69ef4556e32e0d6a4e11365532d8b3fb66c
Marius-Elting <Marius.Supercode@gmail.com>
2023-04-21 16:29:05 +0200

    finsiehd SideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from "react";
-
+import React, { FC, ReactElement } from "react";
 import "./App.css";
-import { FC, ReactElement } from "react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { customTheme } from "./theme/customTheme";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App: FC = (): ReactElement => {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline>
+        <Dashboard />
+      </CssBaseline>
+    </ThemeProvider>
+  );
 };
 
 export default App;
